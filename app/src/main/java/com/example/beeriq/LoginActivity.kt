be757cc1e378e6747d84f4b374a84f52b0b22968
vba16 <vba16@sfu.ca>
2024-11-07 10:45:32 -0800

    connected firebase to project

---
+++ app/src/main/java/com/example/beeriq/LoginActivity.kt
@@ -0,0 +1,14 @@
+package com.example.beeriq
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.viewbinding.ViewBinding
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+    }
+}
