054eae9aabbc73a4d64a8419780cb306e70bfaed
vba16 <vba16@sfu.ca>
2024-11-07 12:37:02 -0800

    login activity checks if username has already been created a blocks user

--- app/src/main/java/com/example/beeriq/LoginActivity.kt
+++ app/src/main/java/com/example/beeriq/LoginActivity.kt
@@ -7,8 +7,11 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.viewbinding.ViewBinding
 import com.example.beeriq.databinding.ActivityLoginBinding
 import com.example.beeriq.databinding.ActivityMainBinding
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
 class LoginActivity : AppCompatActivity() {
 
@@ -30,19 +33,28 @@ class LoginActivity : AppCompatActivity() {
 
             //check if the user has entered a username and password
             if (checkCredentials(tempUser) == 0){
-                //create a unique id for the user
-                val uniqueID = firebaseRef.push().key!!
-                //store the user in the database and let user know account was created
-                firebaseRef.child(uniqueID).setValue(tempUser)
-                    .addOnCompleteListener{
-                        Toast.makeText(this, "Account created", Toast.LENGTH_SHORT).show()
+                //check if the username is unique
+                checkUniqueUsername(tempUser){ exists ->
+                    if (exists){
+                        binding.usernameEditText.error = "Username already exists"
+                    }else{
+                        //create a unique id for the user
+                        val uniqueID = firebaseRef.push().key!!
+                        //store the user in the database and let user know account was created
+                        firebaseRef.child(uniqueID).setValue(tempUser)
+                            .addOnCompleteListener{
+                                Toast.makeText(this, "Account created", Toast.LENGTH_SHORT).show()
+                            }
+                        //continue to the main activity
+                        val intent = Intent(this, MainActivity::class.java)
+                        startActivity(intent)
                     }
-                val intent = Intent(this, MainActivity::class.java)
-                startActivity(intent)
+                }
             }
         }
     }
 
+    //check if the user has entered a username and password *** WIP needs to check if user name is unique
     private fun checkCredentials(user: User) : Int{
         if (user.username?.isEmpty() != false && user.password?.isEmpty() != false){
             binding.usernameEditText.error = "Please enter a username"
@@ -55,6 +67,21 @@ class LoginActivity : AppCompatActivity() {
             binding.passwordEditText.error = "Please enter a password"
             return 1
         }
+
         return 0
+    }
+
+    private fun checkUniqueUsername(user: User, callback:(exists: Boolean) -> Unit): Unit{
+        //check if the username is unique
+        firebaseRef.orderByChild("username").equalTo(user.username)
+            .addListenerForSingleValueEvent(object : ValueEventListener {
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    callback(snapshot.exists())
+                }
+
+                override fun onCancelled(error: DatabaseError) {
+                    callback(false)
+                }
+            })
     }
 }
