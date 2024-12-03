7f7a3b808ce3f35016a769290c11bbf67877b895
Daniel <0624dk@gmail.com>
2024-12-02 22:06:54 -0800

    feat: added searchbar with autocomplete and description activity for breweries

--- app/src/main/java/com/example/beeriq/MainActivity.kt
+++ app/src/main/java/com/example/beeriq/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.beeriq
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.OnBackPressedCallback
@@ -10,6 +11,7 @@ import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.example.beeriq.data.local.breweryDatabase.ImportBreweries
 import com.example.beeriq.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -60,7 +62,6 @@ class MainActivity : AppCompatActivity() {
                 }
             }
         }
-
     }
     override fun onSupportNavigateUp(): Boolean {
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
---
+++ app/src/main/java/com/example/beeriq/ui/map/BreweryDetails.kt
@@ -0,0 +1,31 @@
+package com.example.beeriq.ui.map
+
+import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.example.beeriq.R
+
+class BreweryDetails : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.brewery_details)
+
+        // Get the data passed via the Intent
+        val breweryName = intent.getStringExtra("brewery_name") ?: "Unknown"
+        val breweryAddress = intent.getStringExtra("brewery_address") ?: "Unknown Address"
+        val breweryDescriptionTitle = intent.getStringExtra("brewery_description_title") ?: "No Title"
+        val breweryDescriptionBody = intent.getStringExtra("brewery_description_body") ?: "No Description"
+
+        // Find the TextViews and set the text
+        val nameTextView: TextView = findViewById(R.id.bName)
+        val addressTextView: TextView = findViewById(R.id.bAddress)
+        val descriptionTitleTextView: TextView = findViewById(R.id.bDescriptionTitle)
+        val descriptionBodyTextView: TextView = findViewById(R.id.bDescriptionBody)
+
+        nameTextView.text = breweryName
+        addressTextView.text = breweryAddress
+        descriptionTitleTextView.text = breweryDescriptionTitle
+        descriptionBodyTextView.text = breweryDescriptionBody
+    }
+}
